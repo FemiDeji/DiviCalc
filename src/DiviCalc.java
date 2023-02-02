06f33b5720fe5aee60af67ab6eca018314ee642b
FemiDeji <adeniyioluwafemiayodeji@gmail.com>
2023-02-02 14:43:04 -0800

    Initial commit

---
+++ src/DiviCalc.java
@@ -0,0 +1,11 @@
+public class DiviCalc{
+    public static void main(String[] args){
+      int a, b, c, d;
+      a=200;
+      b=2;
+      c=2;
+      d=2;
+      int division = a/b/c/d;
+      System.out.println(division);
+    }
+}
